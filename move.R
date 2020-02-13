27f9caaca828f0dd3f6f12655634d7ccf3c08fa7
Steve Simon <mail@pmean.com>
2020-02-13 15:57:43 -0600

    Created draft overviews for modules 3-6

---
+++ move.R
@@ -0,0 +1,28 @@
+display_info <- function(x_list, x_name) {
+  n <- length(x_list)
+  if (n==0) {return(0)}
+  cat(length(x_list))
+  cat(" ")
+  cat(x_name)
+  cat(" file(s) moved: ")
+  cat(paste(x_list, collapse=", "))
+  cat("\n")
+}
+
+for (project_name in paste0("module0", 1:6)) {
+  src_path <- paste(project_name, "src", sep="/")
+  res_path <- paste(project_name, "results", sep="/")
+
+  html_list <- list.files(src_path, pattern="*.html")
+  pdf_list <- list.files(src_path, pattern="*.pdf")
+  pptx_list <- list.files(src_path, pattern="*.pptx")
+  for (i in c(html_list, pdf_list, pptx_list)) {
+    fn <- paste(src_path, i, sep="/")
+    file.copy(fn, res_path, overwrite=TRUE)
+    file.remove(fn)
+  }
+  cat(paste0("\n", project_name, "\n"))
+  display_info(pdf_list,  "pdf")
+  display_info(html_list, "html")
+  display_info(pptx_list, "pptx")
+}
